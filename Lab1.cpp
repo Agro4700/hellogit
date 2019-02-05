a807cee23ed153401a77af605da2967a6f981672
Agro4700 <Agro4700@users.noreply.github.com>
2019-02-05 11:17:05 -0400

    Added source file

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-main()
-{
-	vector<int> v1 = {20, 30, 50, 60, 45, 89}
+int main()
+{    
+	//declare
+	string str1 = "hello ";
+	string str2 = str1 + "world";
+	
+	//print
+	cout <<"str1 = "<<str1<<endl;
+	cout <<"str2 = "<<str2<<endl;
+	cout <<"the 4th character is "<<str1[3]<<endl;
+	cout <<"str1 has "<<str1.size()<<" characters"<<endl;
+	
+	//comparison coperators
+	if (str1 == "hello")
+		cout<< "hi there"<<endl;
+		
+	if (str2 != "world")
+		cout<<"out of this world"<<endl;
+	
+	//append to a string
+	str2 +="ly one"; 
+	cout<<" "<<str2<<endl;
+
 }
+
